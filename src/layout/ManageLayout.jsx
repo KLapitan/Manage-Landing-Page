723563c353a459e12dc37bc480467b726b60a0f3
KLapitan <karllapitan5@gmail.com>
2026-08-17 16:00:02 +0800

    edit thenbandageds

--- src/layout/ManageLayout.jsx
+++ src/layout/ManageLayout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
 return(
 <div className="relative overflow-x-hidden max-h-auto h-screen border-4 border-green-800  ">
 
-<div className="absolute z-0 w-90 h-90  -top-10 -right-20  ">
+<div className="absolute z-0 w-90 h-90  -top-10  -right-20  md:w-130 md:h-130  md:-right-5 md:-top-18 ">
 {/* bandage like pattern   */}
 <picture >
 <img src="/images/bg-tablet-pattern.svg" alt="pattern-logo" className="w-full h-full"/>
@@ -17,13 +17,14 @@ return(
 
 </div>
 
-<div className="absolute z-2 w-70 h-80  -bottom-24 -right-42  ">
+<div className="absolute z-2 w-70 h-80  -bottom-24 -right-42  md:hidden">
 {/* bandage like pattern   */}
 <picture >
 <img src="/images/bg-tablet-pattern.svg" alt="pattern-logo" className="w-full h-full"/>
 </picture>
-
 </div>
+  
+
 
 <div className="relative z-10 w-full sm:flex sm:flex-col sm:items-center   ">
 <Header/>
@@ -33,6 +34,13 @@ return(
 <Footer/> 
 </div>
 
+ {/* largeview left side */}
+<div className="absolute z-2 w-133 h-130  md:-bottom-183  md:-left-53 ">
+{/* bandage like pattern   */}
+<picture >
+<img src="/images/bg-tablet-pattern.svg" alt="pattern-logo" className="w-full h-full"/>
+</picture>
+</div>
 </div>
 
 )
